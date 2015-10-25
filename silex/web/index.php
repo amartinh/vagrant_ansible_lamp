8e8a82f22943cb292db49e2fd3535cccbd113ba4
Toni M <tmartinh@gmail.com>
2015-10-25 22:33:35 +0100

    Redistributing files

--- silex/web/index.php
+++ silex/web/index.php
@@ -1,9 +1,11 @@
 <?php
 
 // web/index.php
-require_once __DIR__.'/../vendor/autoload.php';
+require_once __DIR__.'../vendor/autoload.php';
 
 $app = new Silex\Application();
+// Turning on debugging
+$app['debug'] = true;
 
 $app->get('/hello/{name}', function ($name) use ($app) {
     return 'Hello '.$app->escape($name);
